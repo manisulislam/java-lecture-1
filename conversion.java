94f9d15d8f938c963c09300482fc1777f664ba38
anisul islam <anisulislam980340@gmail.com>
2023-05-13 09:30:22 +0600

    java conversion completed

---
+++ conversion.java
@@ -0,0 +1,18 @@
+class conversion
+{
+    public static void main(String args[]){
+        byte b=125;
+        int a=b;
+
+        int c=257;
+        byte k=(byte)c;
+
+        //byte operation
+        byte number_one= 10;
+        byte number_two=30;
+        int result= number_one*number_two;
+        System.out.println(a);
+        System.out.println(k);
+        System.out.println(result);
+    }
+}
--- hello.java
+++ hello.java
@@ -1,8 +1,10 @@
 class hello
 {
     public static void main(String a[]) {
-        System.out.println(2+3);
-        System.out.println(5+8);
+        int num_one=2;
+        int num_two=5;
+        int result= num_one+num_two;
+        System.out.println(result);
         
     }
 }
